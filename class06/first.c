b506b05fcac96534a6d0c2813147ff634ed20a91
osskario <oskarsnaudins@gmail.com>
2022-09-30 11:33:07 +0300

    Class_06

---
+++ class06/first.c
@@ -0,0 +1,19 @@
+// komentāra viena rinda - //tiek apzīmēts komentāra sākums
+//komentāra cita rinda
+
+/*
+Komentāra paragrāfs
+izmanto, piemēram, lai komentētu fragmentus
+*/
+
+int main()
+ {
+ //if()
+  //{
+  //
+  //}
+
+ return 0;
+
+// return 100;
+ }
