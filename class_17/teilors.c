c4b8dd959f67efff4a911dfd405858240589f55c
osskario <oskarsnaudins@gmail.com>
2022-12-02 11:46:50 +0200

    Class_

---
+++ class_17/teilors.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+ double mana_eksponente (double x ){
+  double a,S;
+  int k=0;
+
+  a = pow() //reizinājums
+  S = a;
+   printf("%.2f\t%8.2f\n",x,a,S)
