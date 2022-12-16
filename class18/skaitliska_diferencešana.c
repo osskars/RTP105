0dec82d1542f1c7e4c1e08e61ac29eec91250a06
osskario <oskarsnaudins@gmail.com>
2022-12-16 10:47:29 +0200

    Class_22

---
+++ class18/skaitliska_diferencešana.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+void main ()
+{
+ float a=0.,b=2*M_PI,x,delta_x=1.e-2;
+ // float y;
+ printf("\tx\t\tsin(x)\t\tsin\(x)\n");
+ x = a;
+ while(x<b)
+{
+   //y = sin(x);
+  printf("%10.2f\n",x);
+  //printf("%10.2f\t%10.2f\n",x,y);
+  x+= delta_x;//x = x + delta_x;
+ }
+   {
+  char word[] = "massive";
+    printf("The object is %s.\n", word);
+    return 0;
+ }
+}
