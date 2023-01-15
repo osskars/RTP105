ea78acf567934a1b49a05ccd8ab5d649852af1cb
osskars <113163214+osskars@users.noreply.github.com>
2023-01-15 14:50:28 +0200

    Add files via upload
    Uzrakstiet kodu dialogam ar lietotāju. Lietotājam ir jāievada divi skaitļi - 400 000 (četri simti tūkstoši) un 100 000 (simts tūkstoši), programmai ir jāattēlo reizināšanas operācijas rezultāts - 40 000 000 000 (četrdesmit miljardi).

---
+++ divu skaitļu reizinājums.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int first_number, second_number;
+
+    //pirmais skaitlis
+    printf("Enter the first number:");
+    scanf("%d", &first_number);
+
+    //otrais skaitlis
+    printf("Enter the second number:");
+    scanf("%d", &second_number);
+
+    //reizinajums
+    int result = first_number * second_number;
+
+    //attelojums
+    printf("The result is%d\n",result);
+
+    return 0;
+}
