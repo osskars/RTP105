e72c1a3069b5c36a047db7c241f96d40e12a4277
osskario <oskarsnaudins@gmail.com>
2023-01-18 21:14:14 +0200

    Class_https://github.com/osskars/RTR105

---
+++ class07/scanf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main ()
+{
+  char str [80];
+  int i;
+
+  printf ("Enter your full name: ");
+  scanf ("%79s",str);
+  printf ("Enter your age: ");
+  scanf ("%d",&i);
+  printf ("Mr. %s , %d years old.\n",str,i);
+  printf ("Enter any hexadecimal number: ");
+  scanf ("%x",&i);
+  printf ("You have entered %#x (%d).\n",i,i);
+  printf ("Good luck Mr. %79s");
+  scanf ("%79s",str);
+
+  return 0;
+}
