3752eb197f34e5e6c66eaf84b5c74bb385100217
osskario <oskarsnaudins@gmail.com>
2023-01-23 15:17:05 +0200

    Class_classH7

--- Lab4/skaitliskā_integrēšana.c
+++ Lab4/skaitliskā_integrēšana.c
@@ -1,18 +1,53 @@
-#include<stdio.h>
-#include<math.h>
+#include <stdio.h>
+#include <math.h>
 
 int main(void) {
-  float a=0.,b=M_PI/2,eps=1.e-3,h,integr1=0.,integr2;
-  int k,n=2;
-
-  integr2=(b-a)*(exp(a)+exp(b))/n; 
-  while(fabs(integr2-integr1)>eps){
-    n*=2;
-    h=(b-a)/n;
-    integr1=integr2;
-    integr2=0;
-    for(k=0;k<n;k++)
-    integr2+=h*exp(a+(k+0.5)*h);
-  }
-  printf("Integrāļa vērtība: %.2f\n",integr2);
+    double a,b,c, sk1=0,sk2=0,sk3=0,sk4=0;
+    int k,n=2;
+    //vērtībām
+    printf("Lūdzu ievadiet pirmo vērtību: ");
+    scanf("%lf", &a);
+    printf("\nLūdzu ievadiet otro vērtību: ");
+    scanf("%lf", &b);
+    printf("\nLūdzu ievadiet precizitātes vērtību: ");
+    scanf("%lf", &c);
+    double h = (b-a)/n;
+    sk2=(b-a)*(exp(a)+exp(b))/n;
+    while(fabs(sk2-sk1)>c){
+        n*=2;
+        h = (b-a)/n;
+        sk1=sk2;
+        sk2=0;
+        for(k=0;k<n;k++)
+            sk2 += h*exp(a+(k+0.5)*h);
+    }
+    //Taisnstūru metode
+    printf("Integrālis taisnstūru metodei: %f\n",sk2);
+    n = 2;
+    h = (b-a)/n;
+    sk3 = (b-a)*(exp(a)+exp(b))/n;
+    while(fabs(sk3-sk1)>c){
+        n*=2;
+        h = (b-a)/n;
+        sk1=sk3;
+        sk3=0;
+        for(k=0;k<n;k++)
+            sk3 += (h*exp(a+k*h) + h*exp(a+(k+1)*h))/2;
+    }
+    //Trapeču metode
+    printf("Integrālis trapeču metodei: %f\n",sk3);
+    n = 2;
+    h = (b-a)/n;
+    sk4 = (b-a)*(exp(a)+exp(b))/n;
+    while(fabs(sk4-sk1)>c){
+        n *= 2;
+        h = (b-a)/n;
+        sk1 = sk4;
+        sk4 = 0;
+        for(k=0;k<n;k++)
+            sk4 += (h/6)*(exp(a+k*h) + 4*exp(a+(k+0.5)*h) + exp(a+(k+1)*h));
+    }
+    //Simpsona metode
+    printf("Integrālis simpsona metodei: %f\n", sk4);
+    return 0;
 }
